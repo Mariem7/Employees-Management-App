63ca1518d37110d08444679d9ed9a926f6e967fd
Mariem Neili <mariemneili7@gmail.com>
2022-04-12 22:48:47 +0100

    init Project

---
+++ Backend-Employee-Management/src/main/java/cimf/BackendEmployeeManagement/BackendEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package cimf.BackendEmployeeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEmployeeManagementApplication.class, args);
+	}
+
+}
